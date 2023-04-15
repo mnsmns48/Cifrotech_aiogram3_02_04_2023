332829595adffa55d75ff0059a5f0c9538e019b9
mnsmns48 <getzboy@list.ru>
2023-04-15 21:37:33 +0300

    function refinement

--- distrib_mail_parsing.py
+++ distrib_mail_parsing.py
@@ -2,6 +2,7 @@ import asyncio
 import base64
 import email
 import imaplib
+import quopri
 import time
 
 from openpyxl.reader.excel import load_workbook
@@ -27,48 +28,54 @@ def mail_connect():
 
 
 def mail_processing(msg_list):
-    to_be_write_into_db = list()
     for i in msg_list:
+        to_be_write_into_db = list()
+        encodings_name_list = list()
         result, data = con.fetch(i, "(RFC822)")
         msg = email.message_from_bytes(data[0][1])
         date_time_letter = date_out(email.utils.parsedate_to_datetime(msg["Date"]))
         subject = decode_header(msg["Subject"])[0][0].decode()
-        if hidden_vars.mail_connect.subject_keywords_xls in subject:
-            if msg.is_multipart():
-                for part in msg.walk():
-                    if part.get_content_maintype() == 'multipart' or part.get('Content-Disposition') is None:
-                        continue
-                    filename = part.get_filename()
-                    transfer_encoding = part.get_all('Content-Transfer-Encoding')
-                    if transfer_encoding and transfer_encoding[0] == 'base64':
-                        filename_parts = filename.split('?')
-                        filename = base64.b64decode(filename_parts[3]).decode(filename_parts[1])
-                        if '.xlsx' or '.xls' in filename:
-                            with open(
-                                    f'shippers/{hidden_vars.mail_connect.mail_path}/{filename}', 'wb'
-                            ) as new_file:
-                                new_file.write(part.get_payload(decode=True))
-                            y.upload(f'shippers/{hidden_vars.mail_connect.mail_path}/{filename}',
-                                     f'/shippers/Mobex/{filename}', overwrite=True)
-                            to_be_write_into_db.append([filename, date_time_letter])
+        l = list()
+        for part in msg.walk():
+            if hidden_vars.mail_connect.subject_keywords_xls in subject:
+                if msg.is_multipart():
+                    for part in msg.walk():
+                        if part.get_content_maintype() == 'multipart' or part.get('Content-Disposition') is None:
+                            continue
+                        filename = 'Content-Transfer-Encoding'
+                        transfer_encoding = part.get_all('Content-Transfer-Encoding')
+                        if transfer_encoding and transfer_encoding[0] == 'base64':
+                            filename_parts = filename.split('?')
+                            filename = base64.b64decode(filename_parts[3]).decode(filename_parts[1])
+                            if '.xlsx' or '.xls' in filename:
+                                with open(
+                                        f'shippers/{hidden_vars.mail_connect.mail_path}/{filename}', 'wb'
+                                ) as new_file:
+                                    new_file.write(part.get_payload(decode=True))
+                                y.upload(f'shippers/{hidden_vars.mail_connect.mail_path}/{filename}',
+                                         f'/shippers/Mobex/{filename}', overwrite=True)
+                                to_be_write_into_db.append([filename, date_time_letter])
                 return to_be_write_into_db
-            else:
-                print('Письмо подходит по условию:', hidden_vars.mail_connect.subject_keywords,
-                      'но в нём нет вложений')
-        # if hidden_vars.mail_connect.subject_keywords_apple in subject:
-        #     text = str()
-        #     for part in msg.walk():
-        #         if part.get_content_maintype() == 'text' and part.get_content_subtype() == 'plain':
-        #             text = base64.b64decode(part.get_payload()).decode()
-        #     in_list = list()
-        #     out_list = list()
+            if hidden_vars.mail_connect.subject_keywords_apple in subject:
+                for part in msg.walk():
+            #     l.append(part.get_all('Content-Transfer-Encoding'))
+            # print(l)
+            #     if part.get_content_maintype() == 'text' and part.get_content_subtype() == 'plain':
+            #         transfer_encoding = part.get_all('Content-Transfer-Encoding')
+            #         if transfer_encoding[1] == 'base64':
+            #             text = base64.b64decode(part.get_payload()).decode()
+            #         if transfer_encoding[1] == 'quoted-printable':
+            #             text = quopri.decodestring(part.get_payload()).decode('utf-8')
+            # print(text)
+        #
+
         #     for line in text.split('\n'):
         #         if '  -  ' or ' - ' in line:
         #             in_list.append(line)
         #     for line in in_list:
         #         out_list.append([line.split('  -  ')[0], int(line.split('  -  ')[1])])
         #     filename = hidden_vars.mail_connect.subject_keywords_apple.replace(' ', '_') \
-        #                + '_' + date_time_letter.replace(' ', '_').replace(':', '-') + '.xlsx'
+        #                + '_' + date_time_letter.replace(' ', '_').replace(':', '-') + '.xlsx']=
         #     wb = Workbook()
         #     ws = wb.active
         #     ws.title = "Лист1"
@@ -81,8 +88,8 @@ def mail_processing(msg_list):
         #     to_be_write_into_db.append([filename, date_time_letter])
         #     print(to_be_write_into_db)
         #     return to_be_write_into_db
-        else:
-            print('Новые письма были, но они не подходят под условия')
+        # else:
+        #     print('Новые письма были, но они не подходят под условия')
 
 
 def check_data_in_distributor(date, distributor_price_list):
@@ -155,10 +162,10 @@ def mail_parsing():
         response = mail_connect()
         if response:
             mail_processing(response)
-            prepare_letters = mail_processing(response)
-            if prepare_letters:
-                from_xls_into_db(prepare_letters)
-        print('ухожу в сон на 5 сек')
+        #     if prepare_letters:
+        #         from_xls_into_db(prepare_letters)
+        # print('ухожу в сон на 5 сек')
+        print('5 sek')
         time.sleep(5)
 
 
